34323c85ced8c6daa290d9e497b0634326ed2a3f
bezurton <53045960+bezurton@users.noreply.github.com>
2026-07-10 19:26:15 +0300

    Фикс багов с открытым логином и паролем

--- php/src/db.php
+++ php/src/db.php
@@ -5,13 +5,19 @@ require __DIR__ . '/vendor/autoload.php';
 // подключение к mongodb, используется во всех местах, где нужна работа с базой
 function getMongoCollection(): \MongoDB\Collection
 {
-    $uri = getenv('MONGO_URI') ?: 'mongodb://admin:admin_password@mongodb:27017';
+    $uri = getenv('MONGO_URI');
+    if (!$uri) {
+        throw new \Exception('MONGO_URI not found');
+    }
     $client = new MongoDB\Client($uri);
     return $client->utilities->payments;
 }
 function getHistoryCollection(): \MongoDB\Collection
 {
-    $uri = getenv('MONGO_URI') ?: 'mongodb://admin:admin_password@mongodb:27017';
+    $uri = getenv('MONGO_URI');
+    if (!$uri) {
+        throw new \Exception('MONGO_URI not found');
+    }
     $client = new MongoDB\Client($uri);
     return $client->utilities->history;
 }
